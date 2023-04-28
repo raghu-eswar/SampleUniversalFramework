87b1e31c860481f5e3c887d056acf62e56ac05a5
raghu_eswar <raghu.esawr@fisdom.com>
2023-04-28 22:28:00 +0530

    add framework

---
+++ HelloWorld/HelloWorld.xcframework/ios-arm64_x86_64-simulator/HelloWorld.framework/Headers/HelloWorld.h
@@ -0,0 +1,18 @@
+//
+//  HelloWorld.h
+//  HelloWorld
+//
+//  Created by Raghu Eswar on 28/04/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for HelloWorld.
+FOUNDATION_EXPORT double HelloWorldVersionNumber;
+
+//! Project version string for HelloWorld.
+FOUNDATION_EXPORT const unsigned char HelloWorldVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HelloWorld/PublicHeader.h>
+
+
